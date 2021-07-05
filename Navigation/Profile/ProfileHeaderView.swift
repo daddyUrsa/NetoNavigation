24a6a990ab5190452003db29ea112386a26a3cf2
daddyUrsa <0057747@gmail.com>
2021-07-05 23:10:37 +0300

    Make base UI.

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,96 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Alexey Pavlov on 05.07.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    let avatarImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "alexey")
+        image.layer.cornerRadius = 75
+        image.clipsToBounds = true
+        image.layer.borderWidth = 3
+        image.layer.borderColor = UIColor.white.cgColor
+        
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.text = "Alexey Pavlov"
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .gray
+        label.text = "Waiting for something..."
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let showStatusButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show status", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 4
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func buttonPressed() {
+        print("Pressed")
+    }
+    
+    func setupViews() {
+        addSubview(avatarImage)
+        addSubview(nameLabel)
+        addSubview(descriptionLabel)
+        addSubview(showStatusButton)
+        
+        NSLayoutConstraint.activate([
+            avatarImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+            avatarImage.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            avatarImage.widthAnchor.constraint(equalToConstant: 150),
+            avatarImage.heightAnchor.constraint(equalToConstant: 150),
+            nameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: 27),
+            showStatusButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
+            showStatusButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
+            showStatusButton.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 16),
+            showStatusButton.heightAnchor.constraint(equalToConstant: 50),
+            descriptionLabel.bottomAnchor.constraint(equalTo: showStatusButton.topAnchor, constant: -34),
+            descriptionLabel.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: 27)
+        ])
+        
+    }
+}
---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Alexey Pavlov on 05.07.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    let profileHeaderView = ProfileHeaderView()
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        
+        view.backgroundColor = .lightGray
+        view.addSubview(profileHeaderView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        profileHeaderView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+    }
+}
